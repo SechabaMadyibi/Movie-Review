7d68af0f47e7dec239ecabd60af9cb8ffec14c62
SechabaMadyibi <sechabamadyibi1@gmail.com>
2023-09-16 21:26:29 +0200

    16 september

--- api/movies.controller.js
+++ api/movies.controller.js
@@ -1,4 +1,5 @@
 import MoviesDAO from '../dao/moviesDAO.js'
+
 export default class MoviesController {
     // When apiGetMovies is called via a URL, there will be a query string in the response object (req.query)
     // where certain filter parameters might be specified and passed in through key-value pairs
@@ -8,12 +9,17 @@ export default class MoviesController {
             parseInt(req.query.moviesPerPage) : 20
         const page = req.query.page ? parseInt(req.query.page) : 0
         let filters = {}
+
+    // We then check if the rated query string exists, then add to the filters object.
+   //  We do the same for title
         if (req.query.rated) {
             filters.rated = req.query.rated
         }
         else if (req.query.title) {
             filters.title = req.query.title
         }
+// We next call getMovies in MoviesDAO that we have just implemented. Remember that getMovies will return
+// moviesList and totalNumMovies
         const { moviesList, totalNumMovies } = await
             MoviesDAO.getMovies({ filters, page, moviesPerPage })
         let response = {
@@ -22,7 +28,9 @@ export default class MoviesController {
             filters: filters,
             entries_per_page: moviesPerPage,
             total_results: totalNumMovies,
+            
         }
+        //We then send a JSON response with the above response object to whoever calls this URL
         res.json(response)
     }
 }
--- api/movies.route.js
+++ api/movies.route.js
@@ -3,6 +3,7 @@ import MoviesController from './movies.controller.js'
 const router = express.Router() // get access to express router
 router.route('/').get(MoviesController.apiGetMovies)
 
+
 export default router
 
 // import express from 'express'
