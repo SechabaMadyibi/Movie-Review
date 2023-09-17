5fe2ca570438c4fb095179de506f565af493dfae
SechabaMadyibi <sechabamadyibi1@gmail.com>
2023-09-17 16:55:29 +0200

    17 September

--- api/movies.route.js
+++ api/movies.route.js
@@ -2,8 +2,14 @@ import express from 'express'
 import MoviesController from './movies.controller.js'
 const router = express.Router() // get access to express router
 router.route('/').get(MoviesController.apiGetMovies)
+import ReviewsController from './reviews.controller.js'
 
-
+router
+ .route("/review")
+ .post(ReviewsController.apiPostReview)
+ .put(ReviewsController.apiUpdateReview)
+ .delete(ReviewsController.apiDeleteReview)
+ 
 export default router
 
 // import express from 'express'
